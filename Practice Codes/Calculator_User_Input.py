302398cc56e18e0dbf3c5499abf4230c64981699
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-18 21:44:48 +0530

    Create Calculator_User_Input.py

---
+++ Practice Codes/Calculator_User_Input.py
@@ -0,0 +1,11 @@
+#Calculator User Input
+
+Num1 = input("Enter Your First Number 1 : ") #Input No 1
+Num2 = input("Enter Your Second Number 2 : ") #Input No 2
+
+print("Your Addition Of ",Num1,"And" ,Num2, "Is :",int(Num1)+ int(Num2))#Addition
+print("Your Substraction Of ",Num1,"And" ,Num2, "Is :",int(Num1)- int(Num2))#Subtraction
+print("Your Multiplication Of ",Num1,"And" ,Num2, "Is :",int(Num1)* int(Num2))#Multiplication
+print("Your Division Of ",Num1,"And" ,Num2, "Is :",int(Num1)/ int(Num2))#Division
+
+print("Thank You!!!!")
