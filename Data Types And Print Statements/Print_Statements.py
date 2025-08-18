614d2ec57a97973a43681b387548b1f6926d94b8
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-18 21:41:43 +0530

    Create Print_Statements.py

---
+++ Data Types And Print Statements/Print_Statements.py
@@ -0,0 +1,5 @@
+print("A","F","T","A","B", sep="_")
+# This Print Statement Is Ideally Used For Seprating The Strings By A Character
+
+print("A","F","T","A","B", end="0666")
+# This Print Statement Is Used For Add A End Word Or Number Or Any Other Character To The END oF the String
