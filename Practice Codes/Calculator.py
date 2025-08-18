325de2253ef4d003cae77d4ff1f71d9e08b5789c
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-18 21:44:08 +0530

    Create Calculator.py

---
+++ Practice Codes/Calculator.py
@@ -0,0 +1,9 @@
+Num1 = 10
+Num2 = 20
+
+print("The Addition Of Num1 And Num2 Is : ", Num1+Num2)
+print("The Substraction Of Num1 And Num2 Is : ", Num1-Num2)
+print("The Multiplication Of Num1 And Num2 Is : ", Num1*Num2)
+print("The Division Of Num1 And Num2 Is : ", Num1/Num2)
+
+#This Is A Simple Calculator Made Using Python
