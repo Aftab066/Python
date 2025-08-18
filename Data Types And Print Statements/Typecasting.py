dd57839ce6c67ce24ae0306cf7dda4dc25ca3bf8
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-18 21:42:48 +0530

    Create Typecasting.py

---
+++ Data Types And Print Statements/Typecasting.py
@@ -0,0 +1,19 @@
+# The Typecasting In Python Is Like Coverting One Data Type To Another Like This
+
+a = "20"
+b = "30"
+
+print(int(a) + int(b))
+
+# There Are 2 Types Of Typecasting
+# 1) Explicit Typecasting
+# 2) Implicit Typecasting
+
+#Explicit Typecasting Is Which Is Converted By Developer Or Programmer
+#Implicit Typecasting Is Which Is Converted By Python Interpreter 
+
+#Like This 
+a = 1.8
+b = 8
+
+print(a + b )
