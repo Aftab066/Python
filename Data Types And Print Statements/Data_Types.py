99377c7870ee1f3e564e71dd8cf370afbd776d54
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-18 21:42:15 +0530

    Create Data_Types.py

---
+++ Data Types And Print Statements/Data_Types.py
@@ -0,0 +1,22 @@
+a = 1
+b = 9
+print(a+b)
+#This Is Int Data Type
+
+a = 0.9
+b = 9.1
+print(a+b)
+#This Is Float Data Type
+
+a = "Aftab"
+print(a)
+#This Is String Data Type
+
+a = True
+b = False
+print(a)
+#This Are Boolean Data Types 
+
+dict = {"name" : "Aftab", "Age" :20, "Can Vote" : True}
+print(dict)
+#This Is Dictonary Data Type
