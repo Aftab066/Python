fc5f68a6912fc25b3228286465190fff54c05f25
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-19 10:09:21 +0530

    Create String_Slicing.py

---
+++ Strings/String_Slicing.py
@@ -0,0 +1,12 @@
+#To Know The Length Of A String We Use len Function For It
+
+Nlength = "Aftab"
+print(len(Nlength))
+
+#To Access The Specific Character   Or A Specific Name In A String Here We Do It Like This
+#String Slicing
+ 
+Names = "Aftab , Omkar , Nikhil"
+print(Names[8:13])
+#Negative Slicing
+print(Names[-8:-5])
