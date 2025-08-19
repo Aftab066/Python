ab7c2f4e6be3c8000589536c5df15d6d60c2731c
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-19 10:08:25 +0530

    Create Strings_Basic.py

---
+++ Strings/Strings_Basic.py
@@ -0,0 +1,17 @@
+#This Inline String
+
+print("Hello Coder")
+
+#This Is MultiLine String
+#Triple Quote Is Used For Multiline String
+
+print('''Hello Coder 
+Lets
+Make
+Code Using
+Python''')
+
+#Accesing Character In Strings
+
+Name = "Aftab"
+print(Name[2])
