acd1397c15b8d1607835345f128774885a7078ee
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-20 20:45:43 +0530

    Create Strings_Methods.py

---
+++ Strings/Strings_Methods.py
@@ -0,0 +1,57 @@
+Text = "Aftab"
+#To Convert It Into Uppercase
+print(Text.upper())
+
+#To Convert It Into Lowercase
+print(Text.lower())
+
+#To Spilit The String Into A List
+print(Text.rsplit())
+
+#To Center The String 
+print(Text.center(50))
+
+#To Capitilize The First Character Of String
+print(Text.capitalize())
+
+#To Spilit The String Into A List
+print(Text.replace("Aftab" ,"Nikhil"))
+
+#To count The Characters Of It
+print(Text.count("f"))
+
+#To Spilit The String Into A List
+print(Text.endswith("b"))
+
+#To Find A Word Into String
+print(Text.find("ta"))
+
+#It Is Also Similiar As Find Method IF Statement Does Not Match It Gives Error
+# print(Text.index("Ftabb"))
+
+# It Tells That The String Is In Lowerspace Or Not
+print(Text.islower())
+
+#If The String Lies Between A-Z Or a-z Or 0-9 It Gives A Boolean Output True Otherwise False
+print(Text.isalnum())
+
+#If The String Lies Between A-Z Or a-z  It Gives A Boolean Output True Otherwise False
+print(Text.isalpha())
+
+#It Shows If The String Is Printable Or NOT
+print(Text.isprintable())
+
+#It Prints Space
+print(Text.isspace())
+
+#It Shows True Is The String's First Character Is Capital Or Not If Not It Prints False
+print(Text.istitle())
+
+#It is Same AS Endswith
+print(Text.startswith("A"))
+
+#It Swap The Cases Like Upper To Lower And Lower To Upper
+print(Text.swapcase())
+
+#It Converts The String Into Title Format
+print(Text.title())
