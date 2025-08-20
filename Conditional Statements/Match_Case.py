49d0c1271f39060015deb4fed5415296204fb101
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-20 20:48:51 +0530

    Create Match_Case.py

---
+++ Conditional Statements/Match_Case.py
@@ -0,0 +1,29 @@
+Month =int(input("Enter Month Number:"))
+
+match(Month):
+    case(1):
+        print("The Month Is January!!!")
+    case(2):
+        print("The Month Is Febuary!!!")
+    case(3):
+        print("The Month Is March!!!")
+    case(4):
+        print("The Month Is April!!!")
+    case(5):
+        print("The Month Is May!!!")
+    case(6):
+        print("The Month Is June!!!")
+    case(7):
+        print("The Month Is July!!!")
+    case(8):
+        print("The Month Is August!!!")
+    case(9):
+        print("The Month Is September!!!")
+    case(10):
+        print("The Month Is October!!!")
+    case(11):
+        print("The Month Is November!!!")
+    case(12):
+        print("The Month Is December!!!")
+    case _:
+        print("Invalid Format")
