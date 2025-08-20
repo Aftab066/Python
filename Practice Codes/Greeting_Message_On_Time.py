3e7f701f3636a9ac3a72928b516a5531d5dfcbad
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-20 20:48:04 +0530

    Create Greeting_Message_On_Time.py

---
+++ Practice Codes/Greeting_Message_On_Time.py
@@ -0,0 +1,13 @@
+import time
+timestamp = int(time.strftime('%H'))
+print(timestamp)
+
+if(timestamp>= 6 and timestamp<= 11
+):
+    print("Good Morning")
+elif (timestamp>=12 and timestamp<=16):
+         print("Good Afternoon")
+elif (timestamp>=17):
+         print("Good Evening")
+else:
+    print("Good Night")
