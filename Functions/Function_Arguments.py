48df1dc16000d2a3fcb813daa0fc3f33e94182d5
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-21 20:59:28 +0530

    Create Function_Arguments.py

---
+++ Functions/Function_Arguments.py
@@ -0,0 +1,41 @@
+# Default Arguments
+
+# def average(a=10,b=10):
+#     print("The Average is ",(a+b)/2)
+
+# average()
+
+#So Basically Default Arguments Is That The Default Values Are Given In The Function
+#We Can Also Overwrite It
+
+#Required Arguments
+
+# def average(a,b=10):
+#     print("The Average is ",(a+b)/2)
+
+# average()
+
+# Required Arguments Means If There Are Two Values To Be Given For The Operation
+# Both Values Should Be Present
+
+#Keyword Arguments 
+
+# def average(a=10,b=10):
+#     print("The Average is ",(a+b)/2)
+
+# average(b=5,a=5)
+
+#In This Argument We Can Also Change The Order Block AS Shown Above
+
+# Variable Length Arguments
+
+def Numbers(*num):
+    sum=0
+    for i in num:
+     sum = sum + i
+    print("The Average Is ",sum/len(num))
+
+
+Numbers(5,6)
+
+#Sometimes We Have To Add More Arguments Then We Use This
