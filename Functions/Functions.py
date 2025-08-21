6662d6ecb8cb8ed2f5997be2fbda35a3a2aa19b3
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-21 20:58:49 +0530

    Create Functions.py

---
+++ Functions/Functions.py
@@ -0,0 +1,14 @@
+# Functions
+def Candrive(Age):
+    if(Age>=18):
+     print("You Are Above 18 You Can Drive")
+    elif(Age<18):
+     print("You Are Not Above 18 You Cannot Drive")
+    else:
+     print("Thank You!!!!!")
+
+Candrive(Age = 10) #This Is Called Function Calling
+
+def Isgreater(a,b):
+    pass
+#The Pass Function Is Used If Want To Create Function Later
