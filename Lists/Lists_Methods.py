5d540246f0fc7e23bdc25992e29a58cb1052a0cd
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-22 21:49:17 +0530

    Create Lists_Methods.py

---
+++ Lists/Lists_Methods.py
@@ -0,0 +1,21 @@
+#Append
+l =[10,20,30,60,90,50,20]
+# l.append(666)
+
+#Sort
+# l.sort()
+# l.sort(reverse = True) #It Makes It In Desecending Order
+
+#Reverse
+# l.reverse()
+
+#Index
+# print(l.index(20))
+
+#Copy
+# M = l.copy()
+# print(M)
+
+#Concanated Two List
+M=[666,1111,777]
+print(l+M)
