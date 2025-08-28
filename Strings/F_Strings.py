0e21d4c0b758b84341d7b7472d3f26d3b14570f6
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-28 20:33:47 +0530

    Update

---
+++ Strings/F_Strings.py
@@ -0,0 +1,10 @@
+#F-String Is A String Formating Like This
+# Name = "Aftab"
+# Country = "India"
+# # print(f"My Name Is {Name}, I am From {Country}")
+
+#Therefore The F Is Use For String Formatting 
+#Second Method For String Formatting Is Given Below
+
+text=("My Name Is {}, I am From {}")
+print(text.format("Aftab","India"))
