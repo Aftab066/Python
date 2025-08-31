27e1bd2f660f591738e64da8919f02557566352a
Aftab Mulani <aftabmulani1739@gmail.com>
2025-08-31 20:25:51 +0530

    Update

---
+++ Set/Sets.py
@@ -0,0 +1,11 @@
+# Sets Is Bacically A Collection Of Item 
+Items = {"Aftab",666,True}
+
+#For An Empty Set We Use This
+
+Collection = set()
+
+print(Collection)
+print(Items)
+
+#It is A Unodered Collection
