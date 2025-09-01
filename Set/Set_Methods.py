1567ad60e3150d13770e718911415e8608953a71
Aftab Mulani <aftabmulani1739@gmail.com>
2025-09-01 20:35:39 +0530

    Update

---
+++ Set/Set_Methods.py
@@ -0,0 +1,46 @@
+#Union
+#It Means To Combine Both Sets
+Items1 = {"Area1","Area2","Area3","Area4"}
+Items2 = {"Area5","Area6","Area7","Area8"}
+
+# Items3=Items1.union(Items2)
+
+#Intersection
+#It Means Same Items In Sets
+# Items3=Items1.intersection(Items2)
+
+
+#Symmetric 
+# Items3=Items1.symmetric_difference(Items2)
+
+#Difference
+
+# Items3=Items1.difference(Items2)
+
+#isjoint
+
+# Items3=Items1.isdisjoint(Items2)
+
+#isSubset
+
+
+# Items3=Items1.issubset(Items2)
+
+#issuperset
+# Items3=Items1.issuperset(Items2)
+
+#Add
+# Items1.add(666)
+
+#remove/discard
+# Items1.remove("Area1")
+
+#clear
+# Items1.clear()
+
+#pop
+# Items1.pop()
+
+#delete
+del Items2
+print(Items2)
