ffde30e5954c07bcb6a67b59f0544806b359315e
Aftab Mulani <aftabmulani1739@gmail.com>
2025-09-02 21:06:13 +0530

    Update

---
+++ Dictonaries/Dictonaries.py
@@ -0,0 +1,26 @@
+#Dictonaries Basic
+Dic = {"Name": "Aftab",
+       "Mob" : 123456,
+       "Address": "Satara"
+  }
+
+  
+# print(Dic["Name"])
+# print(Dic.keys())
+# print(Dic.get("Name"))
+# print(Dic.values())
+
+for key, value in Dic.items():
+    print(f"The Keys {key} And The Values {value}")
+
+
+#Example
+
+Result ={"Aftab",
+    "Maths" : 75,
+    "Biology" : 78,
+    "Chemistry" : 76,
+    "Physics" : 77,
+  }
+
+print(Result)
