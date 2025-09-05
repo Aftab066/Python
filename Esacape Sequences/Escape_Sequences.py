07d5a46faf8b49073ed712fd90218637517ab216
Aftab Mulani <aftabmulani1739@gmail.com>
2025-09-05 19:18:26 +0530

    Update

--- Esacape Sequences/Escape_Sequences.py
+++ Esacape Sequences/Escape_Sequences.py
@@ -4,3 +4,4 @@ print("Hey I am Coder \nThis Is A Python Code")
 # This Is Second Escape Sequence Method
 print('Python \"C\"C++')
 # The Above Is Used For Using Qoutes In The Middle Of String
+#Thanks
