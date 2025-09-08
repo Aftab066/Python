1ae612f6e85f703425fa0f3f51fc6409d790e805
Aftab Mulani <aftabmulani1739@gmail.com>
2025-09-08 21:07:02 +0530

    Update

---
+++ Error Handling/Except_Keyword.py
@@ -0,0 +1,12 @@
+# Multiplication Table
+
+Num = (input("Enter The Number :"))
+print(f"The Multiplication Table Of {Num} is :")
+
+try:
+    for i in range(1 , 11):
+     print(f"{int(Num)} X {i} = {int(Num)*i}")
+except:
+    print("Invalid Input")
+
+print("Stop")
