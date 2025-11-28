a47e6ca5f46ccb7c11c837319bdf0f668b9b7701
Aftab Mulani <aftabmulani1739@gmail.com>
2025-11-28 21:16:31 +0530

    Update

---
+++ Raising Custom Errors/Raising_Custom_Errors.py
@@ -0,0 +1,9 @@
+name = input("Enter the person's name that Aftab love :")
+   
+if(name == "Asma" or name == "Ammi"):
+    print("Aftab Loved It")
+elif(name == "quit" or name == "Quit" ):
+    exit()
+else :
+    raise ValueError("You Are Wrong")
+
