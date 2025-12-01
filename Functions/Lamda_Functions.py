56bb3670290dd2e2fc658fcfe1314dadad3ed042
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-01 21:17:30 +0530

    Update

---
+++ Functions/Lamda_Functions.py
@@ -0,0 +1,14 @@
+
+#Lamda Function is to Wrap a simple function in one lined function 
+# For example 
+
+# def cube(x):
+#     x = x*x*x
+#     return x 
+
+# print(cube(5))
+
+# so this is a simple function that we make but this can be written in one lined like this 
+
+cube= lambda  x: x*x*x 
+print(cube(2))
