1b3f68a66ebdb4c8f958dbc945485d72c154a03c
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-02 22:10:35 +0530

    Update

--- Practice Codes/Snake_Water_Game.py
+++ Practice Codes/Snake_Water_Game.py
@@ -5,12 +5,15 @@ results = [
     [0,0,0],
     [0,0,0]
     ]
-
-
+score = []
+row = 0
+loose = []
+draw = []
 #Section 2 : Make Choices 
 
 print("Welcome To Snake-Water-Game!!")
 usrn = input("Your Pretty Name Please : ") 
+print("You Have To Won Atleast 2 Rounds To Win This Game ")
 
 print(f"Hello {usrn} Lets Play The Game :")
 
@@ -18,9 +21,9 @@ print(f"Hello {usrn} Lets Play The Game :")
 import random
 
 #Section 3 : Game Logic
-row = 0
+
 for i in range(3):
-    usrc = input("Choose One \n  1. Snake\n 2. Water \n 3. Gun\n 4. Exit () \n:")
+    usrc = input("Choose One \n  1. Snake 2. Water  3. Gun 4. Exit () :")
     
     options = ("Snake" , "Water" , "Gun")
     computer_c = random.choice(options)
@@ -32,4 +35,50 @@ for i in range(3):
         results [row] [0] = 1
     row += 1
 
-print(results)
+
+
+#Section 3 : Results 
+
+
+for row1 in range(3):
+    if (results [row1] [0] ==1):
+        score.append(results[row1][0])
+    elif(results [row1] [1] == 1):
+        loose.append(results[row1][1])
+    elif(results [row1] [2] == 1):
+        draw.append(results[row1][2])
+
+    
+from functools import reduce
+
+if not score:
+    pass
+else :
+    score = reduce(lambda x, y: x+y, score )
+
+#Loose
+if not loose:
+    pass
+else :
+    loose = reduce(lambda x, y: x+y, loose )
+
+#Draw
+if not draw:
+    pass
+else :
+    draw = reduce(lambda x, y: x+y, draw )
+
+print(score)
+
+if (score==2):
+    print(f"You Won {score} Rounds!!!")
+    print("You Are The Winner!!")
+elif(loose==2):
+    print(f"You Loss {loose} Rounds!!!")
+    print("You Lost The Game")
+elif(draw==2):
+    print(f"{draw} Rounds Are Draw")
+    print("It Was A Tie")
+else:
+    print("Please Play Again")
+    exit()
