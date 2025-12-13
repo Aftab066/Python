aad928643818c46efcc440baab527e53137315b3
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-13 21:48:16 +0530

    Update

---
+++ Practice Codes/Clear_Clutter.py
@@ -0,0 +1,18 @@
+import os
+
+path = r"C:\Users\admin\OneDrive\Pictures\Saved Pictures\PPt Matrieal"
+fn=0
+for file in os.listdir(path):
+    
+    if file.endswith(".png"):
+        fn+=1
+        old_path = os.path.join(path,file)
+        new_path = os.path.join(path,f"Image{fn}.png")
+        os.rename(old_path,new_path)
+        print(file)
+    elif file.endswith(".jpg"):
+        fn+=1
+        old_path = os.path.join(path,file)
+        new_path = os.path.join(path,f"pic{fn}.jpg")
+        os.rename(old_path,new_path)
+        print(file)
