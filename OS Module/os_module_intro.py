d5ac21298af4822981030791c7e7577d552c5738
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-13 20:59:31 +0530

    Update

--- OS Module/os_module_intro.py
+++ OS Module/os_module_intro.py
@@ -2,11 +2,14 @@ import os
 
 Temporary = "Os List"
 
-# os.mkdir(Temporary)
+# os.mkdir(Temporary) #To Create File
 
-# print(f"The Folder {Temporary} Created Successflly")
+print(f"The Folder {Temporary} Created Successflly")
 
-# print(dir("It's My Folder"))
+print(dir("It's My Folder"))
 
-# os.rename(Temporary, "Temped File")
+# os.rmdir(Temporary) #To Delete Empty Directory
+# os.rename(Temporary) #To rename  Directory
+#os.gcwd to Get Current Working Directory
+
 
---
+++ OS Module/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+os.mkdir(Temporary)
+
+print(f"The Folder {Temporary} Created Successflly")
+
+print(dir("It's My Folder"))
+
+os.rename(Temporary, "Temped File")
