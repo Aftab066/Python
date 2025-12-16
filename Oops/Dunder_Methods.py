6ed47d2f5dd613da4b212440b6ef7ca814817498
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-16 10:50:11 +0530

    Update

---
+++ Oops/Dunder_Methods.py
@@ -0,0 +1,20 @@
+#__init__ :Called As Constructor
+#__len__ : To Get Len Of Object
+#__str__ : 
+#__repr__ :
+#__call__ : To Make Object Calalable
+
+class Employee:
+    def __init__(self,name):
+        self.name = name
+        
+    def __len__(self):
+        i = 0
+        for c in self.name:
+            i = i + 1
+            
+        return i
+        
+a = Employee("Aftab")
+print(a.name)
+print(len(a))
