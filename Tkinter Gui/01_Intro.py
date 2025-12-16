1f0f1e69b9f22ab51461b4fc64cc9c2ac20b1d39
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-16 22:04:23 +0530

    Update

---
+++ Tkinter Gui/01_Intro.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk() #Creates Window  
+root.title("Aftab") #Title For The Window
+root.geometry("500x300") #Size Of Screen
+Aftab = Label(root,text="Aftab Is GOAT") #Text In Window
+Aftab.pack()
+root.mainloop() #Keeps Open The Screen
+
--- Tkinter Gui/tempCodeRunnerFile.py
+++ Tkinter Gui/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-import tkinter as tk
-
-def clicked():
-    print("Alhamdulillah!")
-
-root = tk.Tk()
-root.title("Asma Mulani")
-
-btn = tk.Button(root, text="Click Me", command=clicked)
-btn.pack()
-
-root.mainloop()
+root = mainloop()
