6298bdd6e6d378e8611a6aee233481da12d43079
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-23 21:28:40 +0530

    Update

--- Practice Codes/Shoutout_To_Everyone.py
+++ Practice Codes/Shoutout_To_Everyone.py
@@ -4,10 +4,15 @@ speaker_number = 0
 spk = wincl.Dispatch("SAPI.SpVoice")
 vcs = spk.GetVoices()
 # SVSFlag = 11
-print(vcs.Item (speaker_number) .GetAttribute ("Name")) # speaker name
+# print(vcs.Item (speaker_number) .GetAttribute ("Name")) # speaker name
 spk.Voice
 spk.SetVoice(vcs.Item(speaker_number)) # set voice (see Windows Text-to-Speech settings)
-spk.Speak("Hey I Am Jarvis")
-spk.Speak("Welcome To Aftab's Lab")
-spk.Speak("Here I Am Your Assistant")
-spk.Speak("All Out")
+lst = ["Shoutout To Aftab",
+       "Shoutout To Omkar",
+       "Shoutout To Nikhil"]
+# spk.Speak("Hey I Am Jarvis")r
+# spk.Speak("Welcome To Aftab's Lab")
+for i in lst:
+    print(i)
+    spk.Speak(i)
+# spk.Speak("All Out")
