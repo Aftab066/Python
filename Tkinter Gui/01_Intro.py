b0f6ee9d33d6d6b05d8192cddacaa9f79a84290e
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-25 21:17:15 +0530

    a

--- Tkinter Gui/01_Intro.py
+++ Tkinter Gui/01_Intro.py
@@ -1,9 +1,12 @@
 from tkinter import *
+import time
 
 root = Tk() #Creates Window  
 root.title("Aftab") #Title For The Window
 root.geometry("500x300") #Size Of Screen
 Aftab = Label(root,text="Aftab Is BEST") #Text In Window
 Aftab.pack()
+
+
 root.mainloop() #Keeps Open The Screen
 
