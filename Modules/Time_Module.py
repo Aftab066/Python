dd32fd45424b1256f830530019db56587e081866
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-25 19:57:06 +0530

    t

---
+++ Modules/Time_Module.py
@@ -0,0 +1,21 @@
+# Time Module Is Used To Display Time
+#time.time Is used for displaying Time In Seconds From 1970 Its The Date When time module was introduced
+
+import time
+
+# print(time.time())
+
+#time.sleep is used for pausing time for any custom time in seconds
+
+# print("Hey see you after 3 seconds")
+# time.sleep(3)
+# print("I'm Here ")
+
+#time.ctime() for displaying current time in human readable format
+# print(time.ctime())
+
+# time.localtime() :- For Local Time format
+
+# t = time.localtime()
+# print(t.tm_hour,":",t.tm_min,":",t.tm_sec)
+
