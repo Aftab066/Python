40560e59f69e8a0f6b68a27d750c161f0f65b4ec
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-26 20:08:23 +0530

    X

---
+++ Modules/Walrus_Operator.py
@@ -0,0 +1,3 @@
+# Walrus Operator Value Ko Assign Karta Hai Aur Sath Me Return Bhi Karta Hai
+while (msg := input("Enter The Message To Display It \n To Quit Type (Exit):  ")) != "Exit":
+    print("Your Mesaage :- \n ",msg)
