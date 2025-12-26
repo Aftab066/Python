f14f499b1f3ab132a6e9d0f6ce281d2bd6ce2de8
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-26 20:30:00 +0530

    a

--- Modules/Shutil_Module.py
+++ Modules/Shutil_Module.py
@@ -1,1 +1,8 @@
 # shutil module is used for performing high level file operations
+
+import shutil
+
+# shutil.copy("myfile.txt","Myfile1.txt") #To Copy File
+# shutil.copytree("myfile.txt","Myfile1.txt") #To Copy Whole Folder
+# shutil.move("myfile.txt","Myfile1.txt") #To Move The file
+# shutil.rmtree("myfile.txt","Myfile1.txt") #To delete a directory
