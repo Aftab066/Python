8b4bc44503d264a8cdf26639ed36ed2a62f1e6aa
Aftab Mulani <aftabmulani1739@gmail.com>
2025-12-30 14:00:39 +0530

    k

--- Practice Codes/News_App.py
+++ Practice Codes/News_App.py
@@ -1,18 +1,31 @@
 import requests
 
 url = "https://gnews.io/api/v4/search?q=example&apikey=API_KEY"
+usr = ""
 
 params = {
-    "q": "india",
+    "q": "Stock market",
     "lang": "en",
     "country": "in",
     "max": 5,
-    "apikey": "4186e3d7433060de6c06d3d766ae02ef"   # 👈 YAHAN API KEY
+    "apikey": "4186e3d7433060de6c06d3d766ae02ef"   #YAHAN API KEY
 }
 
-response = requests.get(url, params=params)
-data = response.json()
+print("Hello And Welcome To Aaj Tak \n ")
+print("1.General News \n 2.World \n 3. Business \n 4.Technology \n 5.Nation \n 6.Entertainment \n 7.Sports \n 8.Science \n 9.Health \n 10. Exit()  ")
 
-for news in data["articles"]:
-    print(news["Title"])
+
+
+usr = input("Enter The Topic Name  To Search The News ")
+params["q"] = usr # 
+
+if usr == "Exit" or usr == "exit":
+    exit()
+else:
+    response = requests.get(url, params=params)
+    data = response.json()
+    for news in data["articles"]:
+     print(news["title"])
+
+
 # print(data)
