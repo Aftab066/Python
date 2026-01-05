b73d71ffd658a7b449f09ce7c3ffb89db8002620
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-05 21:01:23 +0530

    v

---
+++ Multiprocessing/Mutliprocessing.py
@@ -0,0 +1,22 @@
+#Mutliprocessing : Is Same Like Multithreading But It Runs On Processors
+
+import multiprocessing
+import requests
+
+def Downloader(url,name):
+    print(f"Started Downloading {name}")
+    response = requests.get(url)
+    open(f"files/file{name}.jpg","wb").write(response.content)
+    print(f"Finished Downloading{name}")
+    
+if __name__ == "__main__":
+    url = "https://picsum.photos/200/300"
+    pros = []
+    for i in range(10):
+        p = multiprocessing.Process(target=Downloader,args=[url,i])
+        p.start()
+        pros.append(p)
+    
+    for p in pros:
+        p.join()
+
