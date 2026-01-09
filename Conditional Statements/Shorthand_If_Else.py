530c3186207f106b0904376182fe2a2b4abe6e5b
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-09 22:05:14 +0530

    Add thank you comment at the end of the script
    Added a comment thanking the user at the end of the script.

--- Conditional Statements/Shorthand_If_Else.py
+++ Conditional Statements/Shorthand_If_Else.py
@@ -7,3 +7,4 @@ x = int(input("Enter The First Integer Of Your Phone No :"))
 y = int(input("Enter The Last Integer Of Your Phone No :"))
 
 print(f"{x} is Greater ") if x>y else print(f"{y} is greater ") if x<y else print("Equal")
+#Thanks
