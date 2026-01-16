1db7d65bc454e8068fff684922e67980a8a51302
Aftab Mulani <aftabmulani1739@gmail.com>
2026-01-16 20:58:39 +0530

    q

--- Practice Codes/Contact_Book.py
+++ Practice Codes/Contact_Book.py
@@ -18,7 +18,7 @@ introduction()
 
 while True:
     print("1_Add")
-    print("2_Seach")
+    print("2_Search")
     print("3_Update")
     print("4_Delete")
     print("5_Exit")
@@ -41,9 +41,25 @@ while True:
             print(f'Name : {name},age : {Age}, Mob : {Mob_No},Email :{email}')
         else:
             print("Not Found")
+    elif usr=="update":
+        name = input("Enter the Name Which You Want To Update:")
+        if name in contact_Book:
+            name = input("Enter Updated Name :")
+            Mob_No = int(input("Enter Updated  Mob No :"))
+            email = input("Enter Updated Email :")
+            Age = int(input("Enter Updated Age :"))
+            contact_Book[name] = (f'Name : {name},age : {Age},Mob : {Mob_No},Email :{email}')
+            print("Updated Successfully")
+        else :
+            print("Not Found")
+    elif usr=="delete":
+        name = input("Enter The Name To Delete :")
+        if name in contact_Book:
+            del contact_Book[name]
+            print("Deleted Successfully!!!")
     elif usr == "exit":
         exit()
-    print(contact_Book)
+    
 
 
 
