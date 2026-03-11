5262fb22d8e393ebf23587e913a14fb1c7bf0ee3
Aftab Mulani <aftabmulani1739@gmail.com>
2026-03-11 19:48:04 +0530

    s

--- Functions/Args_Kwargs.py
+++ Functions/Args_Kwargs.py
@@ -1,7 +1,21 @@
 '''Args :- Arguments or Positional Arguments
 Kwargs :- Keyword Arguments''' 
 
-def employe(*name): #Args Take Multiple Positinal Arguments denoted by *
-    print(name)
+# def employe(*name): #Args Take Multiple Positinal Arguments denoted by *
+#     print(name)
     
-employe("aftab",'omkar','ismail')
+# employe("aftab",'omkar','ismail')
+
+def Company(**employe): #Takes Multile Keyword Arguments
+    print(employe)
+    
+Company(name="Aftab",Age=21,ID=666,City="Satara")
+
+def Math(*Num):
+    total = 0
+    for i in Num:
+        total+=i
+    return total
+
+print(Math(10,20,30,40,50,60))
+
