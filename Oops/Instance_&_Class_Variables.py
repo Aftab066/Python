ab42520e7d4bd94fe5fc7f2166c463a51d26909d
Aftab Mulani <aftabmulani1739@gmail.com>
2026-03-11 19:58:49 +0530

    e

--- Oops/Instance_&_Class_Variables.py
+++ Oops/Instance_&_Class_Variables.py
@@ -13,7 +13,7 @@ class YC:
     
 s1 = YC("Aftab","CS")
 s1.clg = "YC Satara" #Update Only For s1 Object The changes are applied before calling the function
-s1.id() # After Func executes the default changes are applied
+s1.id() # After Method executes the default changes are applied
 s2 = YC("Omkar","Chemistry") 
 s2.id()
 
