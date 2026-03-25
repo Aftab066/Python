c0de9c2b080d8629d0631b3597a402d127b8e31f
Aftab Mulani <aftabmulani1739@gmail.com>
2026-03-25 19:50:21 +0530

    q

---
+++ Assignments/Assignment_01/01_Fundamentals.py
@@ -0,0 +1,99 @@
+#Question 1
+"""name = input("Enter Your Pretty Name :")
+Age = int(input("Enter Your Age :"))
+
+print(f"Hi {name}, You are {Age} years old")
+"""
+#Question 2
+
+'''num1 = float(input("Enter The Num 1 :"))
+num2 = float(input("Enter The Num 2 :"))
+
+print("Sum :",num1+num2)
+print("Diff :",num1-num2)
+print("Product :",num1*num2)
+print(" Quotient :",num1/num2)
+'''
+
+#Question 3
+
+'''a = int(input("Enter Int only :"))
+b = int(input("Enter Int only :"))
+c = float(input("Enter float only :"))
+
+print("Num 1 Before : ",type(a),a)
+print("Num 1 After : ",type(float(a)),a)
+
+print("Num 2 Before : ",type(b),b)
+print("Num 2 After : ",type(float(b)),b)
+
+print("Num 3 Before : ",type(c),c)
+print("Num 3 After : ",type(c),c)
+'''
+
+#Question 4
+
+'''x = input("Enter The Number :")
+
+print(int(x))
+print(float(x))
+print(str(x))'''
+
+#Question 5
+
+'''x = 10+3*2**2
+
+print(x)
+'''
+'''It Will Apply Bodmass/Pemdas Rule Means
+Paranthesis
+Exponential
+Multiplication
+Division
+Addition
+Subtraction'''
+
+#Qustion 6
+
+'''a = int(input("Enter Num 1 :"))
+b = int(input("Enter Num 2 :"))
+
+print("Num 1 After Swaped :",b)
+print("Num 2 After Swaped :",a)'''
+
+#Question 7
+
+'''temp = input("Enter The Temperature :")
+
+f_temp = (float(temp) *(9/5)) + 32
+
+print("Temperature In Farhenheit : ",(f_temp),'f')'''
+
+#Question 8
+
+'''r = int(input("Enter The Radius :"))
+pi = 3.14
+
+Area = pi * r**2
+
+print("Area : ",Area)'''
+
+#Question 9
+
+'''p = float(input("Enter The Principle To Calculate Simple Interest :"))
+r = float(input("Enter The Rate To Calculate Simple Interest :"))
+t = float(input("Enter The Time To Calculate Simple Interest :"))
+
+si = (p*r*t)/100
+
+print("The Simple Interest : ",si)
+'''
+
+#Question 10
+
+x = float(input("Enter The Num :"))
+z = int(x)
+d = x - z
+print("The Integer Part :",z)
+print("The Float  Part :",d)
+
