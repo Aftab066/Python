de48878031a20b8c8e8a36f4b7f00baaf4637ab5
Aftab Mulani <aftabmulani1739@gmail.com>
2026-03-29 15:18:12 +0530

    z

---
+++ Assignments/Assignment_03/Fundamentals_Part_03.py
@@ -0,0 +1,30 @@
+#Question 1
+
+'''usr = input("Enter The Word : ")
+
+r = usr[::-1]
+
+if (usr==r):
+    print("Its A Palindrome")
+else:
+    print("Not A Palindrome")'''
+    
+#Question 2 :
+
+'''usr = (input("Enter The Numbers : ")).split()
+x = 0
+for i in usr:
+    x+=int(i)
+    final =  x/len(usr)
+    
+print(final)'''
+
+#Question 3 : 
+
+usr1 = (input("Enter The Numbers : ")).split()
+usr2 = (input("Enter The Numbers : ")).split()
+
+final = usr1+usr2
+final.sort()
+
+print(final)
