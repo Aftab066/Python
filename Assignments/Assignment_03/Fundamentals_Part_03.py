a96552e77ef954dde6680bc90c0eda60863794aa
Aftab Mulani <aftabmulani1739@gmail.com>
2026-03-29 16:37:27 +0530

    a

--- Assignments/Assignment_03/Fundamentals_Part_03.py
+++ Assignments/Assignment_03/Fundamentals_Part_03.py
@@ -30,3 +30,56 @@ final.sort()
 print(final)'''
 
 # Question 4 :
+
+'''tup = (1,2,3,4,5,6,7,8,9)
+tup2 = tuple(x for x in tup if x %2==0)
+tup3 = tuple(y for y in tup if y %2==1)
+
+print(tup2)
+print(tup3)
+'''
+#Question 5 :
+'''info = {}
+while True:
+    
+    print("Welcome To Student Managemnet System ")
+    print("\nA: Add Student \n B: Update Marks \n C:Search For Student \n D: Display All Student And Marks\n E: Exit ()")
+
+    usr = input("Enter The Input To Proceed : ").lower()
+    
+    if (usr=='a'):
+       n = input("Enter The Name Of Student :")
+       if(n in info):
+           print("Already Exist")
+       else:
+            m = int(input("Enter The Marks : "))
+            info[n]=m
+            print(f"Added Succesfully{info}")
+    elif(usr=='b'):
+        n = input("Enter The Name Of Student :")
+        if(n in info):
+            m = int(input("Enter The Marks : "))
+            info[n]=m
+            print(f"Updated Marks {info}")
+        else:
+            print("Student Does Not Exists")
+    elif(usr=='c'):
+        n = input("Enter The Name Of Student :")
+        if(n in info):
+            print(f'{n} {info[n]}')
+        else:
+            print("Student Does Not Exists")
+    elif(usr=='d'):
+        if(info=={}):
+            print("No Data Availeble")
+        else:
+            for keys,values in info.items():
+                print(f"{keys} : {values}")
+    elif(usr=='e'):
+        print("Exited Succesfully")
+        break
+    else:
+        print("Enter The VALID input")
+            '''
+            
+#Question 6 :
