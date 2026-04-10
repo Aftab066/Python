e25993838391b18c25c1477edb795676777f4811
Aftab Mulani <aftabmulani1739@gmail.com>
2026-04-10 21:35:42 +0530

    Static Method

--- Practice Codes/Pratice_File.py
+++ Practice Codes/Pratice_File.py
@@ -1,1 +1,15 @@
-pass
+class Store:
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+    
+    @staticmethod
+    def calc_disc(rs,disc):
+        f_price = rs - (disc * rs/100)
+        print(f"The Final Price Is {f_price}")
+        
+s1 = Store("Iphone",85000)
+s1.calc_disc(100000,5)
+
+print(s1.name)
+print(s1.price)
