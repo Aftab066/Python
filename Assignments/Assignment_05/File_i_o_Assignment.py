1457f0c7be5b02d47be5e4c1f2c8fa0616e0ce0a
Aftab Mulani <aftabmulani1739@gmail.com>
2026-04-11 22:26:58 +0530

    Assignment 5 Completed

--- Assignments/Assignment_05/File_i_o_Assignment.py
+++ Assignments/Assignment_05/File_i_o_Assignment.py
@@ -9,7 +9,37 @@
 
 #Question 2 :
 
-with open("log.txt", "a+") as f:
-    f.write("Program run Successfully!!! \n")
-    f.seek(0)
-    print(f.read())
+# with open("log.txt", "a+") as f:
+#     f.write("Program run Successfully!!! \n")
+#     f.seek(0)
+#     print(f.read())
+    
+    
+#Question 3 :
+
+# lst = [5,10,15,20,25]
+
+# n_lst = [val  for val in lst if val > 15]
+
+# print(n_lst)
+
+#Question 4 :
+# import json
+# with open(r"E:\Developer\Git Repo\Python\Assignments\Assignment_05\cities.json","r") as f:
+#     data = json.load(f)
+
+# x = input("Enter The City Name : ")
+# y = int(input("Enter The Population : "))
+# data[x] = y
+
+# with open(r"E:\Developer\Git Repo\Python\Assignments\Assignment_05\cities.json","w") as f:
+#     json.dump(data,f,indent=4)
+
+#Question 5 :
+
+# try:
+#     with open("data.txt",'r') as f:
+#         print(f.read())
+        
+# except FileNotFoundError:
+#     print("File Not Found")
